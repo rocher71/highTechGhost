c924600b93c9a9a45982741ff7ad28369fc6c4af
hyeinKim <sdu07024@naver.com>
2020-10-04 07:21:27 +0900

    4층 클릭이벤트

--- New Unity Project/Assets/Script/GameController.cs
+++ New Unity Project/Assets/Script/GameController.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	IEnumerator Start () {
 
-		yield return new WaitForSeconds (1f);
-
-		DialogDataAlert alert = new DialogDataAlert ("대학원생 김논문(26)", "대학원...오지마........\n살려줘.....", delegate() {
-			Debug.Log("OK Pressed!");
+    void talk()
+    {
+        DialogDataAlert alert = new DialogDataAlert("대학원생 김논문(26)", "대학원...오지마........\n살려줘.....", delegate () {
+            Debug.Log("OK Pressed!");
 
             /*
 			DialogDataConfirm confirm = new DialogDataConfirm("대학원생 김논문(26)", "내가 지금 막학기여서 캡스톤을 하는데.. 파일을 merge하다 충돌이 일어났어.. 오류메세지를 검색하고 싶은데 와이파이 비밀번호를 몰라 구글링하지 못하고있어..", 
@@ -27,9 +25,14 @@ public class GameController : MonoBehaviour {
 			DialogManager.Instance.Push(confirm);
             */
 
-		});
-		DialogManager.Instance.Push (alert);
-	}
+        });
+        DialogManager.Instance.Push(alert);
+    }
+	// Use this for initialization
+	IEnumerator Start () {
+
+        yield return new WaitForSeconds(1f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
